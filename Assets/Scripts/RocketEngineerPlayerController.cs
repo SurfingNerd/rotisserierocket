0caa2975b58095e719f345cc23cb2e86a3ac0853
Thomas Haller <Thomas.Haller@lab10.coop>
2020-02-01 15:20:36 +0100

    Added Skeleton for PlayerController.

---
+++ Assets/Scripts/RocketEngineerPlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketEngineerPlayerController : MonoBehaviour
+{
+
+    public float MinPositionZ = -50;
+    public float MaxPositionZ = 50;
+
+    public float CharacterSpeed = 10; 
+
+
+    float CurrentRotation = 0;
+
+
+    public GameObject WorldRootToRotate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (WorldRootToRotate == null)
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
