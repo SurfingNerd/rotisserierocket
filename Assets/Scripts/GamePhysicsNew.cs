cd3e1be6ffdcd87af5c6e34e7a2c61c6d27fcbc5
Thomas Haller <Thomas.Haller@lab10.coop>
2020-02-02 14:55:01 +0100

    experimental new game  physics.

---
+++ Assets/Scripts/GamePhysicsNew.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePhysicsNew : MonoBehaviour
+{
+    private LevelManager levelManager;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelManager = LevelManager.Inst;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        //Vector3 sidewardVector = new Vector3(0, 0, 0);
+        float x = 0;
+        float y = 0;
+
+        float energyScaling = 1.0f;
+
+        foreach(RocketLeak leak in levelManager.currentRocketStatus.rocketLeaks)
+        {
+            x += leak.transform.position.x;
+            y += leak.transform.position.y;
+        }
+
+        Vector3 forwardVector = new Vector3(x, y, 1.0f);
+
+        // we drag the earth closer.
+        gameObject.transform.position -= (forwardVector * energyScaling * Time.deltaTime);
+
+        Debug.Log("Appying Force: " + forwardVector + " New Earth Position: "  + gameObject.transform.position);
+    }
+}
