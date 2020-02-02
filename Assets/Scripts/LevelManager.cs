75ebb1bef1bec085ff628d424acda95d000fbda2
Amici Nybråten <emilnyb@hotmail.com>
2020-02-02 14:37:11 +0100

    Pre Master Scene Edit

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,31 +2,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class LevelManager : Singleton<LevelManager>
 {
-    public float levelProgress = 0;
+    public Image WinScreen;
+    public Image LoseScreen;
+    public Transform PlayScreen;
 
-    public float levelLengthSeconds = 120;
-    public float startingOxygenKilos = 1141; //1141kg = 1m3 uncompressed oxygen
+    public float maxTimeProgress = 1f;
+    public float timeProgress = 0;
 
-    public float oxygenWarningLevelKilos = 200;
+    public float levelDurationSeconds = 120;
+
+    public float maxOxygen = 1f; //Obsolete comment: 1141kg = 1m3 uncompressed oxygen
+    public float OxygenLevel;
+    public Severity OxygenDepletionSeverity;
+    public int NumLeaks = 0;
+    public float OxygenDepletionPerLeak = 0.1f;
 
     private float elapsedTimeSeconds = 0;
     private float startTime = 0;
-    private float currentOxygenKilos;
-    private bool outOfOxygen = false;
-
-
+    
     public RocketStatus currentRocketStatus;
+
+    public bool ShouldGameRun = false;
 
     // Start is called before the first frame update
     void Start()
     {
         startTime = Time.time;
-        currentOxygenKilos = startingOxygenKilos;
+        OxygenLevel = maxOxygen;
         currentRocketStatus = new RocketStatus();
 
+        StartGame();
     }
 
     // Update is called once per frame
@@ -34,33 +44,52 @@ public class LevelManager : Singleton<LevelManager>
     {
         elapsedTimeSeconds = Time.time - startTime;
 
-        levelProgress = elapsedTimeSeconds / levelLengthSeconds;
+        timeProgress = elapsedTimeSeconds / levelDurationSeconds;
 
-        CheckOxygenLevels();
-
+        ConsumeOxygen(NumLeaks * OxygenDepletionPerLeak);
+        EvaluateOxygenDepletionSeverity();
         CheckLoseConditions();
     }
 
-    private void CheckOxygenLevels()
+    void EvaluateOxygenDepletionSeverity()
     {
-        if(currentOxygenKilos <= 0)
-        {
-            outOfOxygen = true;
-        }
-        else if (currentOxygenKilos <= oxygenWarningLevelKilos)
-        {
-
-        }
+        if (OxygenLevel < 0.2f) OxygenDepletionSeverity = Severity.High;
+        else if (OxygenLevel < 0.4f) OxygenDepletionSeverity = Severity.Medium;
+        else if (OxygenLevel < 0.6f) OxygenDepletionSeverity = Severity.Low;
+        else if (OxygenLevel <= 0f) OxygenDepletionSeverity = Severity.Empty;
     }
 
-    private void CheckLoseConditions()
+    void CheckLoseConditions()
     {
-
+        if(timeProgress >= maxTimeProgress || OxygenDepletionSeverity == Severity.Empty)
+            StartCoroutine(GameOver());
     }
 
-    public void ConsumeOxygen(float kilograms)
+    public void StartGame()
     {
-        currentOxygenKilos -= kilograms;
+        PlayScreen.DOScaleY(0f, 1f);
+        ShouldGameRun = true;
     }
 
+    IEnumerator GameOver()
+    {
+        ShouldGameRun = false;
+        LoseScreen.DOFade(1f, 2f);
+        yield return new WaitForSecondsRealtime(6f);
+        //PlayScreen.DOScaleY(1f, 1f);
+    }
+
+    public void ConsumeOxygen(float depletionValue)
+    {
+        OxygenLevel -= depletionValue;
+    }
+}
+
+public enum Severity
+{
+    None,
+    Low,
+    Medium,
+    High,
+    Empty
 }
